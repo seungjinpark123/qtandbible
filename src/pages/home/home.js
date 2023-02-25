c6c59f36bf46d605dd324603fb006933bce3244f
seungjin park <sparkpsalm@gmail.com>
2023-02-25 16:10:33 +0900

    홈화면 drawer 추가

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,182 @@
-import React from "react";
+import React, {useState} from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
-const Home = ()=>{
-    return(
-        <div>
-           하이루. 홈 페이지입니다.
-        </div>
-    )
+// import MenuIcon from '@mui/icons-material/Menu'; //설치
+// import SearchIcon from '@mui/icons-material/Search';//설치
+// import AccountCircle from '@mui/icons-material/AccountCircle';//설치
+// import MailIcon from '@mui/icons-material/Mail';//설치
+// import NotificationsIcon from '@mui/icons-material/Notifications';//설치
+// import MoreIcon from '@mui/icons-material/MoreVert';//설치
+
+// 스타일
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+
+const Home= () => {
+  const [isDrawerOpen,setIsDrawerOpen] = useState(false)
+  const onBurgerClick = () =>{
+    setIsDrawerOpen(true)
+  }
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+   setIsDrawerOpen(open)
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Home'].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List> 
+      <Divider />
+      <List>
+        {['오늘 수입/지출', '이번달 수입/지출', '올해 수입/지출', '레포트'].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List> 
+      <Divider />
+      <List>
+        {['더보기', 'FAQ', 'Contact'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              {/* <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon> */}
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            onClick={onBurgerClick}
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            2023 가계부 
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              {/* <SearchIcon /> */}
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                {/* <MailIcon /> */}
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                {/* <NotificationsIcon /> */}
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+            anchor={'left'}
+            open={isDrawerOpen}
+            onClose={toggleDrawer('left', false)}
+          >
+            {list('left')}
+          </Drawer>
+      {<div>홈화면 레포트 그래프 보여주기</div>}
+    </Box>
+  );
 }
 
 export default Home;
--- src/pages/qt-mng/daily.js
+++ src/pages/qt-mng/daily.js
@@ -3,7 +3,7 @@ import React from "react";
 const Daily = ()=>{
     return(
         <div>
-            하이루. 데일리 페이지입니다.
+           하이루. 데일리 페이지입니다.
         </div>
     )
 }
